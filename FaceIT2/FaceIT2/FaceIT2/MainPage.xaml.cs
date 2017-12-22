a45a319e59dfc18b7551855aab5c5c51b2a14030
nuwanwixx <nuwan.wixx@gmail.com>
2017-12-22 09:52:57 +0530

    Set project File

---
+++ FaceIT2/FaceIT2/FaceIT2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FaceIT2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            image1.Source = ImageSource.FromResource("FaceIT2.Images.background.jpg");
+        }
+
+        
+
+       
+
+        async void Button_ForgetPassword(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ForgetPasswordPage());
+        }
+
+        async void Button_SignUp(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+
+        async void Button_SignIn(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new SignInPage());
+        }
+    }
+}
---
+++ FaceIT2/FaceIT2/FaceIT2/SignInPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FaceIT2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Login(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProfilePage());
+        }
+    }
+}
