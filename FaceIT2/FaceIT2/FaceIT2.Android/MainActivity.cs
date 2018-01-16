2bd94f8069e0713c987a85189f111fc6bb5d76bb
nadeeshani15 <34505841+nadeeshani15@users.noreply.github.com>
2018-01-16 22:22:33 +0530

    Upload photo

--- FaceIT2/FaceIT2/FaceIT2.Android/MainActivity.cs
+++ FaceIT2/FaceIT2/FaceIT2.Android/MainActivity.cs
@@ -7,18 +7,21 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Xamarin.Forms.Platform.Android;
+using Plugin.Media;
 
 namespace FaceIT2.Droid
 {
     [Activity(Label = "FaceIT", Icon = "@drawable/monkey1", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override async void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+
+            await CrossMedia.Current.Initialize();
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
--- FaceIT2/FaceIT2/FaceIT2/SignUpPage.xaml.cs
+++ FaceIT2/FaceIT2/FaceIT2/SignUpPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -24,10 +25,69 @@ namespace FaceIT2
           }
           */
 
-        private void Button_UploadPhoto(object sender, EventArgs e)
+        private async void Button_UploadPhoto(object sender, EventArgs e)
         {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No camera", ":(No camera available.", "ok");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                }
+                );
+
+            if (file == null)
+                return;
+
+            PathLabel.Text = file.AlbumPath;
+
+            MainImage.Source = ImageSource.FromStream(() =>
+            {
+                var Stream = file.GetStream();
+                file.Dispose();
+                return Stream;
+            });
+        }
+
+
+
+
+
+        private async void Button_Galary(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("oops", "pick a Photo is not supported!", "ok");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            PathLabel.Text = "Photo Path" + file.Path;
+
+            MainImage.Source = ImageSource.FromStream(() =>
+            {
+                var Stream = file.GetStream();
+                file.Dispose();
+                return Stream;
+            });
 
         }
+
+
+
+
         private void Button_Picture(object sender, EventArgs e)
         {
            
@@ -38,6 +98,6 @@ namespace FaceIT2
             await Navigation.PushAsync(new SignUpPage2());
         }
 
-         
+       
     }
 }
